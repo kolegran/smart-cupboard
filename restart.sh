6d28cfbe4658a811c0634bb095230fef5ab4f8a3
kolegran <kolegran@gmail.com>
2019-04-03 23:26:02 +0300

    Bumped up build version number

--- restart.sh
+++ restart.sh
@@ -3,4 +3,4 @@ docker network remove smart-cupboard-net
 
 docker network create smart-cupboard-net
 docker run -d -v /root/data-smart-cupboard:/var/lib/postgresql/data -p 5435:5432 -e POSTGRES_DB=smart-cupboard --network smart-cupboard-net --name smart-cupboard-pg postgres
-docker run -d --network smart-cupboard-net -p 8080:8080 -e DB_HOST=smart-cupboard-pg -e DB_PORT=5432 --name smart-cupboard-backend kolegran/smart-cupboard-backend:latest
+docker run -d --network smart-cupboard-net -p 8080:8080 -e DB_HOST=smart-cupboard-pg -e DB_PORT=5432 --name smart-cupboard-backend kolegran/smart-cupboard-backend:1.0
