ccbd9d20950bca80cdb02fb025bea19e1030ff4f
kebren <sawa3301@gmail.com>
2019-04-06 17:35:38 +0300

    Created itemService and shelfService

--- src/main/java/smartcupboard/github/com/demo/item/ItemService.java
+++ src/main/java/smartcupboard/github/com/demo/item/ItemService.java
@@ -2,30 +2,46 @@ package smartcupboard.github.com.demo.item;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
     private final ItemRepository itemRepository;
 
+    @Transactional(readOnly = true)
     public List<ItemDto> getAll() {
-        return null;
+        return itemRepository.findAll()
+                .stream()
+                .map(ItemDto::new)
+                .collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
     public ItemDto getById(Long itemId) {
-        return null;
+        return new ItemDto(itemRepository.getOne(itemId));
     }
 
+    @Transactional(readOnly = true)
     public List<ItemEventDto> getHistory(Long itemId) {
         return null;
     }
 
+    @Transactional
     public ItemDto create(CreateItemCommand command) {
-        return null;
+        Item item = new Item();
+
+        item.setTitle(command.getTitle());
+        item.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+        return new ItemDto(itemRepository.save(item));
     }
 
+    @Transactional
     public void deleteById(Long itemId) {
+        itemRepository.deleteById(itemId);
     }
 }
