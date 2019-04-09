9419c7f92a0e883e4983a562e5429ed91030719e
kebren <sawa3301@gmail.com>
2019-04-09 22:09:59 +0300

    Implemented create, read, update, delete base functionalities for Reader

--- src/main/java/smartcupboard/github/com/demo/reader/ReaderService.java
+++ src/main/java/smartcupboard/github/com/demo/reader/ReaderService.java
@@ -2,30 +2,51 @@ package smartcupboard.github.com.demo.reader;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import smartcupboard.github.com.demo.cupboard.shelf.sector.SectorRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ReaderService {
-    private ReaderRepository readerRepository;
+    private final ReaderRepository readerRepository;
+    private final SectorRepository sectorRepository;
 
+    @Transactional(readOnly = true)
     public List<ReaderDto> getAll() {
-        return null;
+        return readerRepository.findAll()
+                .stream()
+                .map(ReaderDto::new)
+                .collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
     public ReaderDto getById(Long readerId) {
-        return null;
+        return new ReaderDto(readerRepository.getOne(readerId));
     }
 
+    @Transactional
     public ReaderDto create(CreateUpdateReaderCommand command) {
-        return null;
+        Reader reader = new Reader();
+
+        reader.setTitle(command.getTitle());
+        reader.setSector(sectorRepository.getOne(command.getSectorId()));
+        return new ReaderDto(readerRepository.save(reader));
     }
 
+    @Transactional
     public ReaderDto updateById(Long readerId, CreateUpdateReaderCommand command) {
-        return null;
+        Reader reader = readerRepository.getOne(readerId);
+
+        reader.setTitle(command.getTitle());
+        reader.setSector(sectorRepository.getOne(command.getSectorId()));
+        return new ReaderDto(readerRepository.save(reader));
     }
 
+    @Transactional
     public void deleteById(Long readerId) {
+        readerRepository.deleteById(readerId);
     }
 }
