42a31d9ae8702b730da43c3d7cb31b88521e9747
kebren <sawa3301@gmail.com>
2019-04-09 22:11:08 +0300

    Implemented create, read, delete base functionalities for Item

--- src/main/java/smartcupboard/github/com/demo/item/ItemService.java
+++ src/main/java/smartcupboard/github/com/demo/item/ItemService.java
@@ -1,9 +1,13 @@
 package smartcupboard.github.com.demo.item;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -11,11 +15,14 @@ public class ItemService {
     private final ItemRepository itemRepository;
 
     public List<ItemSimpleDto> getAll() {
-        return null;
+        return itemRepository.findAll()
+                .stream()
+                .map(ItemSimpleDto::new)
+                .collect(Collectors.toList());
     }
 
     public ItemSimpleDto getById(Long itemId) {
-        return null;
+        return new ItemSimpleDto(itemRepository.getOne(itemId));
     }
 
     public List<ItemEventDto> getHistory(Long itemId) {
@@ -23,9 +30,14 @@ public class ItemService {
     }
 
     public ItemSimpleDto create(CreateItemCommand command) {
-        return null;
+        Item item = new Item();
+
+        item.setTitle(command.getTitle());
+        item.setCreatedAt(new Timestamp(new Date().getTime()));
+        return new ItemSimpleDto(itemRepository.save(item));
     }
 
     public void deleteById(Long itemId) {
+        itemRepository.deleteById(itemId);
     }
 }
