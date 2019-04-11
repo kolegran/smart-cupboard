8bf2e881beedd1b28b3553f5481a054d95e18864
kolegran <kolegran@gmail.com>
2019-04-11 04:38:06 +0300

    Updated docker configures

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:11-jre
 
 RUN mkdir /app
-COPY build/libs/demo-0.0.1-SNAPSHOT.jar /app
+COPY build/libs/smart-cupboard-0.0.1-SNAPSHOT.jar /app
 
-ENTRYPOINT ["java", "-jar", "/app/demo-0.0.1-SNAPSHOT.jar"]
+ENTRYPOINT ["java", "-jar", "/app/smart-cupboard-0.0.1-SNAPSHOT.jar"]
--- restart.sh
+++ restart.sh
@@ -1,9 +1,9 @@
+docker pull kolegran/smart-cupboard-backend
+
 docker rm -f smart-cupboard-pg smart-cupboard-backend
 docker network remove smart-cupboard-net
 
 docker network create smart-cupboard-net
-
-docker pull smart-cupboard-backend
 
 docker run -d -v /root/data-smart-cupboard:/var/lib/postgresql/data -p 5435:5432 -e POSTGRES_DB=smart-cupboard --network smart-cupboard-net --name smart-cupboard-pg postgres
 docker run -d --network smart-cupboard-net -p 8080:8080 -e DB_HOST=smart-cupboard-pg -e DB_PORT=5432 --name smart-cupboard-backend kolegran/smart-cupboard-backend:latest
