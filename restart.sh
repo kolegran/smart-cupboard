1556d9e8bbd93a0591c53755c5d110b00f08e487
kolegran <kolegran@gmail.com>
2019-04-22 02:55:16 +0300

    Added mosquitto-broker to restart.sh

--- restart.sh
+++ restart.sh
@@ -5,5 +5,7 @@ docker network remove smart-cupboard-net
 
 docker network create smart-cupboard-net
 
+docker run -it -p 1883:1883 -p 9001:9001 --name=mosquitto-broker --network smart-cupboard-net toke/mosquitto
+
 docker run -d -v /root/data-smart-cupboard:/var/lib/postgresql/data -p 5435:5432 -e POSTGRES_DB=smart-cupboard --network smart-cupboard-net --name smart-cupboard-pg postgres
-docker run -d --network smart-cupboard-net -p 8080:8080 -e DB_HOST=smart-cupboard-pg -e DB_PORT=5432 --name smart-cupboard-backend kolegran/smart-cupboard-backend:latest
+docker run -d --network smart-cupboard-net -p 8080:8080 -e DB_HOST=smart-cupboard-pg -e DB_PORT=5432 -e MQTT_HOST=mosquitto-broker -e MQTT_PORT=1883 --name smart-cupboard-backend kolegran/smart-cupboard-backend:latest
