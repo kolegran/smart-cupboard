b10fd0ee4fc964ffb0c0a5c1b24643395418de1f
kolegran <kolegran@gmail.com>
2019-06-04 20:31:06 +0300

    Updated restart.sh

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 docker pull kolegran/smart-cupboard-backend
-docker pull kolegran/smart-cupboard-front-end
+docker pull kolegran/smart-cupboard-frontend
 
-docker rm -f smart-cupboard-pg smart-cupboard-backend smart-cupboard-front-end mosquitto-broker
+docker rm -f smart-cupboard-pg smart-cupboard-backend smart-cupboard-frontend mosquitto-broker
 docker network remove smart-cupboard-net
 
 docker network create smart-cupboard-net
@@ -9,7 +9,7 @@ docker network create smart-cupboard-net
 docker run -d -v /root/data-smart-cupboard:/var/lib/postgresql/data -p 5435:5432 -e POSTGRES_DB=smart-cupboard --network smart-cupboard-net --name smart-cupboard-pg postgres
 docker run -d --network smart-cupboard-net -p 8080:8080 -e DB_HOST=smart-cupboard-pg -e DB_PORT=5432 -e MQTT_HOST=mosquitto-broker -e MQTT_PORT=1883 --name smart-cupboard-backend kolegran/smart-cupboard-backend:latest
 
-docker run -d --network smart-cupboard-net -e "VIRTUAL_HOST=smart-cupboard.kolegran.com" -e "LETSENCRYPT_HOST=smart-cupboard.kolegran.com" --name smart-cupboard-front-end kolegran/smart-cupboard-front-end
+docker run -d --network smart-cupboard-net -e "VIRTUAL_HOST=smart-cupboard.kolegran.com" -e "LETSENCRYPT_HOST=smart-cupboard.kolegran.com" -e "LETSENCRYPT_EMAIL=kolegran@gmail.com" --name smart-cupboard-frontend kolegran/smart-cupboard-frontend
 docker run -d --network smart-cupboard-net -p 80:80 -p 443:443 -v /root/nginx/certs:/etc/nginx/certs -v /root/nginx/vhost.d:/etc/nginx/vhost.d -v /usr/share/nginx/html -v /var/run/docker.sock:/tmp/docker.sock:ro --name nginx-proxy jwilder/nginx-proxy
 docker run -d --network smart-cupboard-net --volumes-from nginx-proxy -v /var/run/docker.sock:/var/run/docker.sock:ro --name nginx-proxy-letsencrypt jrcs/letsencrypt-nginx-proxy-companion
 
